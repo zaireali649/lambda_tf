93227298fa62aa870b42e28272e263c99a36d2ac
Zaire Ali <35949230+zaireali649@users.noreply.github.com>
2022-07-17 19:39:37 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,16 @@ data "archive_file" "layer_code" {
   output_file_mode = "0666"
   output_path      = "${path.module}/files/lambda_layer.zip"
 }
+
+module "demo_function" {
+  source           = "./modules/lambda"
+  depends_on = [data.archive_file.function_code, data.archive_file.layer_code]
+  name             = "demo-function"
+  filename         = data.archive_file.function_code.output_path
+  source_code_hash = data.archive_file.function_code.output_base64sha256
+
+  create_layer           = true
+  layer_filename         = data.archive_file.layer_code.output_path
+  layer_source_code_hash = data.archive_file.layer_code.output_base64sha256
+  
+}
